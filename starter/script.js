3d0dc0425a79050de64c1c73b5a88122a00305ee
Marta Bala <marta.bala.92@gmail.com>
2023-01-07 15:00:54 +0100

    added some changes to JS however this is not yet functional

---
+++ starter/script.js
@@ -0,0 +1,138 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+//var characterUppercase = document.getElementById('upperCasedCharacters');
+//var characterSpecial = document.getElementById('specialCharacters'); 
+//var characterNumbers = document.getElementById('numericCharacters');
+//var passwordDisplay = document.getElementById("passwordDisplay"); 
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  alert("Your password should contain between 10 and 64 characters");
+  //if (user != null) {
+    //document.getElementById("generate").innerHTML =
+    //"Your password should be between 10-64 characters and include lowercase, uppercase and numeric characters";
+  //}
+}
+getPasswordOptions();
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  
+
+// Function to generate password with user input
+function generatePassword() {
+  var passwordLenght = [10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64];
+  var password = "";
+  prompt("Do you want your password to contain special characters?");
+  if (prompt = "yes") {
+    
+  }
+
+// alert 
+//return random password
+
+}
+
+/// do not change any of the below ??? 
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
